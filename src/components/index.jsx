207ada2631c701471f6750f8415d898713adefe6
Bruna <brunadias_30@hotmail.com>
2023-01-14 19:15:50 -0300

    Criando páginas

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -12,4 +12,4 @@ export { default as LineChart } from './Charts/LineChart';
 export { default as Stacked } from './Charts/Stacked';
 export { default as Pie } from './Charts/Pie';
 export { default as Header } from './Header';
-export { default as ChartsHeader } from '/ChartsHeader';
+export { default as ChartsHeader } from './ChartsHeader';
