55d822800c3a658e0266cf736b8c43e416d30399
Bruna <brunadias_30@hotmail.com>
2023-01-15 22:07:16 -0300

    configurando navbar

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
 import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor, Line} from './pages';
+import { useStateContext } from './contexts/ContextProvider';
 import './App.css'
 
 const App = () => {
-  const activeMenu = true;
+  const { activeMenu } = useStateContext();
   return (
     <div>
       <BrowserRouter>
