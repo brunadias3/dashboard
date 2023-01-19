a3c0f37ad389cfbe03274f4cf63e3cfa7bf122e5
Bruna <brunadias_30@hotmail.com>
2023-01-19 20:41:19 -0300

    fix

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -28,8 +28,8 @@ const UserProfile = () => {
           alt="user-profile"
         />
         <div>
-          <p className="font-semibold text-xl dark:text-gray-200"> Michael Roberts </p>
-          <p className="text-gray-500 text-sm dark:text-gray-400">  Administrator   </p>
+          <p className="font-semibold text-xl dark:text-gray-200"> Bruna Dias </p>
+          <p className="text-gray-500 text-sm dark:text-gray-400">  Developer   </p>
           <p className="text-gray-500 text-sm font-semibold dark:text-gray-400"> info@shop.com </p>
         </div>
       </div>
