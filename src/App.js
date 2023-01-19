48b59d19d2c51b77b53a03563b649f9fd7d238c5
Bruna <brunadias_30@hotmail.com>
2023-01-19 20:26:06 -0300

    theme settings

--- src/App.js
+++ src/App.js
@@ -9,14 +9,14 @@ import { useStateContext } from './contexts/ContextProvider';
 import './App.css'
 
 const App = () => {
-  const { activeMenu } = useStateContext();
+  const { activeMenu, themeSettings, setThemeSettings } = useStateContext();
   return (
     <div>
       <BrowserRouter>
         <div className='flex relative dark:bg-main-dark-bg'>
           <div className='fixed right-4 bottom-4' style={{ zIndex: '1000' }}>
             <TooltipComponent content="Settings" positions="Top">
-              <button type='button' className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white' style={{ background: 'blue', borderRadius: '50%' }}>
+              <button type='button' className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white' style={{ background: 'blue', borderRadius: '50%' }} onClick={() => setThemeSettings(true)}>
                 <FiSettings />
               </button>
             </TooltipComponent>
@@ -38,7 +38,7 @@ const App = () => {
             </div>
 
             <div>
-              <ThemeSettings />
+              {themeSettings && <ThemeSettings />}
               
               <Routes>
                 {/* Dashboard */}
--- src/components/ThemeSettings.jsx
+++ src/components/ThemeSettings.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { MdOutlineCancel } from 'react-icons/md';
 import { BsCheck } from 'react-icons/bs';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
 import { themeColors } from '../data/dummy';
 import { useStateContext } from '../contexts/ContextProvider';
 
@@ -9,22 +10,22 @@ const ThemeSettings = () => {
   const { setColor, setMode, currentMode, currentColor, setThemeSettings } = useStateContext();
 
   return (
-    <div className='bg-half-transparent w-screen fixed nav-item top-0 right-0'>
-      <div className='float-right h-screen dark:text-gray-200 bg-white dark:[#484B52] w-400'>
+    <div className="bg-half-transparent w-screen fixed nav-item top-0 right-0">
+      <div className="float-right h-screen dark:text-gray-200  bg-white dark:bg-[#484B52] w-400">
         <div className="flex justify-between items-center p-4 ml-4">
-          <p className="font-semibold text-xl">Settings</p>
+          <p className="font-semibold text-lg">Settings</p>
           <button
             type="button"
-            onClick={() => { }}
-            style={{ color: 'rgb(153,171,180)', borderRadius: '50%' }}
-            className='text-2xl p-3 hover:drop-shadow-xl hover:bg-light-gray'
+            onClick={() => setThemeSettings(false)}
+            style={{ color: 'rgb(153, 171, 180)', borderRadius: '50%' }}
+            className="text-2xl p-3 hover:drop-shadow-xl hover:bg-light-gray"
           >
             <MdOutlineCancel />
           </button>
-        </div>
 
-        <div className='flex-col border-t-1 border-color p-4 ml-4'>
-          <p className='font-semibold text-lg'> Theme Options</p>
+        </div>
+        <div className="flex-col border-t-1 border-color p-4 ml-4">
+          <p className="font-semibold text-xl ">Theme Option</p>
 
           <div className="mt-4">
             <input
@@ -33,25 +34,23 @@ const ThemeSettings = () => {
               name="theme"
               value="Light"
               className="cursor-pointer"
-              onChange={() => { }}
-              checked={true}
+              onChange={setMode}
+              checked={currentMode === 'Light'}
             />
             {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
             <label htmlFor="light" className="ml-2 text-md cursor-pointer">
               Light
             </label>
           </div>
-
-
-          <div className="mt-4">
+          <div className="mt-2">
             <input
               type="radio"
               id="dark"
               name="theme"
               value="Dark"
+              onChange={setMode}
               className="cursor-pointer"
-              onChange={() => { }}
-              checked={true}
+              checked={currentMode === 'Dark'}
             />
             {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
             <label htmlFor="dark" className="ml-2 text-md cursor-pointer">
@@ -59,18 +58,22 @@ const ThemeSettings = () => {
             </label>
           </div>
         </div>
-        <div className='flex-col border-t-1 border-color p-4 ml-4'>
-          <p className='font-semibold text-lg'> Theme Colors</p>
-          <div classname='flex gap-3'>
+        <div className="p-4 border-t-1 border-color ml-4">
+          <p className="font-semibold text-xl ">Theme Colors</p>
+          <div className="flex gap-3">
             {themeColors.map((item, index) => (
               <TooltipComponent key={index} content={item.name} position="TopCenter">
-                <div className='relative mt-2 cursor-pointer flex gap-5 items-center'>
+                <div
+                  className="relative mt-2 cursor-pointer flex gap-5 items-center"
+                  key={item.name}
+                >
                   <button
-                    type='button'
-                    className='h-10 rounded-full cursor-pointer'
+                    type="button"
+                    className="h-10 w-10 rounded-full cursor-pointer"
                     style={{ backgroundColor: item.color }}
-                    onClick={() => { }}>
-                    <BsCheck className={`ml-2 text-2xl text-white ${false ? 'block' : 'hidden'}`} />
+                    onClick={() => setColor(item.color)}
+                  >
+                    <BsCheck className={`ml-2 text-2xl text-white ${item.color === currentColor ? 'block' : 'hidden'}`} />
                   </button>
                 </div>
               </TooltipComponent>
@@ -79,7 +82,7 @@ const ThemeSettings = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ThemeSettings
+export default ThemeSettings;
